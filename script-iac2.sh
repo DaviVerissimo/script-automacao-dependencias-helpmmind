c674cc2381500123edd7957f0666675f55994091
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2023-10-21 20:44:35 -0300

    Update script-iac2.sh

--- script-iac2.sh
+++ script-iac2.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
 echo "Atualizando o servidor..."
+
 apt-get update
 apt-get upgrade -y
-apt-get install apache2 -y
-apt-get install unzip -y
 
+echo "Instalando dependências..."
+
+# apt-get install apache2 -y
+# apt-get install unzip -y
+sudo apt install git
+sudo apt install nodejs
+sudo apt install npm
+sudo apt install default-jdk
 
 echo "Baixando e copiando os arquivos da aplicação..."
 
-cd /tmp
-wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
-unzip main.zip
-cd linux-site-dio-main
-cp -R * /var/www/html/
+# cd /tmp
+# wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 
-# Instalar java
+git clone https://github.com/DaviVerissimo/HelpMind
+# unzip main.zip
+# cd linux-site-dio-main
+# cp -R * /var/www/html/
+
 # ver questao do mysql 
 
 
